0b56d3b167bb452d0aaad952852a713ae5d580db
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2014-02-10 03:26:03 -0500

    changed facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'JiT6PjAl4MTXCSvRm4aFA', '7URkC9GBIU39CbJMkaFnQilq8qA0H4fAFYITFtYkQv4', :display => "popup"
-  provider :facebook, '587388351349075', 'a276eddaad650f8f5d763217a883e4d8'
+  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret']
 end
